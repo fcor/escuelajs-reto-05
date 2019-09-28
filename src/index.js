38d57de53e1093b165c4eb1e8f71b3958ccb9469
Fabio Cortes <fcor14@hotmail.com>
2019-09-28 10:37:03 -0500

    Primer reto ok

--- src/index.js
+++ src/index.js
@@ -2,23 +2,33 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
 
+const cardTemplate = character => `
+  <article class="Card">
+    <img src="${character.image}" />
+    <h2>${character.name}<span>${character.species}</span></h2>
+  </article>
+  `
+
+const handleHtmlTemplate = characters => {
+  let output = characters.map(character => cardTemplate(character)).join('');
+  let newItem = document.createElement('section');
+  newItem.classList.add('Items');
+  newItem.innerHTML = output;
+  $app.appendChild(newItem);
+}
+
+const handleLocalStorage = info => {
+  localStorage.setItem('next_fetch', info.next);
+}
+
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
       const characters = response.results;
-      let output = characters.map(character => {
-        return `
-      <article class="Card">
-        <img src="${character.image}" />
-        <h2>${character.name}<span>${character.species}</span></h2>
-      </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
+      const info = response.info;
+      handleHtmlTemplate(characters);
+      handleLocalStorage(info);
     })
     .catch(error => console.log(error));
 }
